77e4ec5d0c6d559de94fd49917ceda7eb8e6142c
marilin.moor <marilin.moor@gmail.com>
2026-02-04 15:41:34 +0200

    context file added

--- src/imagentj/imagej_context.py
+++ src/imagentj/imagej_context.py
@@ -1,10 +1,11 @@
 import imagej
 from typing import Optional
+from config.imagej_config import FIJI_JAVA_HOME
 
 _ij_instance: Optional["imagej.ImageJ"] = None
 
 def get_ij():
     global _ij_instance
     if _ij_instance is None:
-        _ij_instance  = imagej.init("/home/marilin/Downloads/Fiji.app", mode='interactive')
+        _ij_instance = imagej.init(FIJI_JAVA_HOME, mode='interactive')
     return _ij_instance
